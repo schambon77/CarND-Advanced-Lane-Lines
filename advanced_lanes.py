af6d2dc063e6219fe4a57682e221465907b86256
schambon77 <30735585+schambon77@users.noreply.github.com>
2017-09-27 22:17:40 -0500

    Implement finding lines

--- advanced_lanes.py
+++ advanced_lanes.py
@@ -8,7 +8,6 @@ import glob
 import os
 
 # Camera calibration
-# ==================
 def get_calibration_matrix_and_distortion_coefs():
     images = glob.glob('./camera_cal/calibration*.jpg')
     objpoints = []
@@ -65,6 +64,7 @@ def hls_select(img, thresh=(0, 255)):
     binary_output[(s > thresh[0]) & (s <= thresh[1])] = 1
     return binary_output
 
+# Apply combined threshold to image
 def threshold_image(img, debug=0, debug_dir='./', img_file='img'):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     s_bin = hls_select(img, thresh=(170, 255))
@@ -83,8 +83,109 @@ def threshold_image(img, debug=0, debug_dir='./', img_file='img'):
         mpimg.imsave(debug_dir + 'thresh_mag_dir_' + img_file, mag_dir_bin, cmap='gray')
     return combined_bin
 
+# Apply perspective transform to image
+def warp_image(img):
+    src = np.float32(
+        [[689, 450],
+         [1109, 719],
+         [187, 719],
+         [591, 450]])
+    dst = np.float32(
+        [[950, 0],
+         [950, img.shape[0]],
+         [300, img.shape[0]],
+         [300, 0]])
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return warped
+
+def find_lines(warped_bin, debug=0, debug_dir='./', img_file='img'):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(warped_bin[np.int(warped_bin.shape[0]*3/4):, :], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((warped_bin, warped_bin, warped_bin)) * 255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(warped_bin.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = warped_bin.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = warped_bin.shape[0] - (window + 1) * window_height
+        win_y_high = warped_bin.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, warped_bin.shape[0] - 1, warped_bin.shape[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    if debug == 1:
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        plt.imshow(out_img)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, out_img.shape[1])
+        plt.ylim(out_img.shape[0], 0)
+        plt.savefig(debug_dir + 'lines_bin_' + img_file)
+        plt.clf()
+
+    return left_fit, right_fit
+
 # Process image pipeline
-# ======================
 def process_image(img, mtx, dist, debug=0, debug_dir='./', img_file='img'):
     # Distortion correction
     undist = cv2.undistort(img, mtx, dist, None, mtx)
@@ -95,13 +196,20 @@ def process_image(img, mtx, dist, debug=0, debug_dir='./', img_file='img'):
         mpimg.imsave(debug_dir + 'thresh_' + img_file, bin, cmap='gray')
 
     # Perspective transform
+    warped = warp_image(img)
+    warped_bin = warp_image(bin)
+    if debug == 1:
+        mpimg.imsave(debug_dir + 'warped_' + img_file, warped)
+        mpimg.imsave(debug_dir + 'warped_bin_' + img_file, warped_bin, cmap='gray')
 
     # Finding the lines
+    find_lines(warped_bin, debug=debug, debug_dir=debug_dir, img_file=img_file)
 
     # Measure curvature
 
+    # Display lane area
+
 # Process test images
-# ===================
 def process_test_images(mtx, dist, debug=0, debug_dir='./'):
     test_files = os.listdir('./test_images')
     for test_file in test_files:
